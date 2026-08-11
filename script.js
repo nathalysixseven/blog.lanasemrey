21df4744c3faab03f3841b975dfb72aecb3c914b
nathalysixseven <nathaly.fernandes31@escola.pr.gov.br>
2026-08-11 09:55:45 -0300

    colocar emoji

---
+++ script.js
@@ -0,0 +1,13 @@
+const botoes = document.querySelectoAll('buttn');
+
+botoes.forEach(botao => {
+    botao.addEventListener('click', curir)
+
+    function curtir () {
+        let curtias = document.querySelecto('span')
+        curtias.textContent++
+        
+    }
+    
+    
+});
